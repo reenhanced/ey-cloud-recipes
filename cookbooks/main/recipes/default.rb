2701ad9fddde4d1da4ab46ab19f00e7ad16df258
Nick Hance <nhance@reenhanced.com>
2015-06-04 16:17:07 -0400

    db name change

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -137,7 +137,7 @@ if ['solo','db_master', 'db_slave'].include?(node[:instance_role])
   # postgresql9_dict_xsyn "dbname"
   # postgresql9_earthdistance "dbname"
   # postgresql9_fuzzystrmatch "dbname"
-  postgresql9_hstore "asgard_production"
+  postgresql9_hstore "asgard"
   # postgresql9_intarray "dbname"
   # postgresql9_isn "dbname"
   # postgresql9_lo "dbname"
