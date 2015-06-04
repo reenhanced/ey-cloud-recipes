72a7da6e4fc280b104c5e754d4412aa619e7742a
Nick Hance <nhance@reenhanced.com>
2015-06-04 16:54:58 -0400

    Setup asgard environment variables

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -87,7 +87,7 @@
 
 #uncomment to set environment variables in passenger or unicorn
 # Set environment variables as specified in cookbooks/env_vars/attributes/env_vars.rb
-#include_recipe "env_vars"
+include_recipe "env_vars"
 
 
 #uncomment to include the mysql_replication_check recipe
