858d595374c5ae58a8e76e985d969ef9c8d6b6cb
Nick Hance <nhance@reenhanced.com>
2015-06-09 18:25:36 -0400

    Rename api-keys-yml to secrets-yml for prep

--- cookbooks/secrets-yml/recipes/default.rb
+++ cookbooks/secrets-yml/recipes/default.rb
@@ -1,7 +1,7 @@
 if ['app_master', 'app', 'util', 'solo'].include?(node[:instance_role])
   node[:applications].each do |app, data|
-    template "/data/#{app}/shared/config/api-keys.yml"do
-      source 'api-keys.yml.erb'
+    template "/data/#{app}/shared/config/secrets.yml"do
+      source 'secrets.yml.erb'
       owner node[:owner_name]
       group node[:owner_name]
       mode 0655
